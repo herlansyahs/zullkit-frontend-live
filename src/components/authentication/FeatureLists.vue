cfaae1f4724657ba8d0a84ca9a60e281e1512efe
herlansyahs <herlansyah.saputra@gmail.com>
2023-01-29 01:25:02 +0700

    Update 29 Januari 2023

---
+++ src/components/authentication/FeatureLists.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Customizable from "./features/Customizable.vue";
+import Documentation from "./features/Documentation.vue";
+import Fortune from "./features/Fortune.vue";
+</script>
+
+<template>
+  <Customizable />
+  <Fortune />
+  <Documentation />
+</template>
